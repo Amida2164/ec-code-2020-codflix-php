02cca2961d3f975a86ee8ec44f96e44bb4fb9be0
Jarno <jremetter21@gmail.com>
2020-06-23 20:44:23 +0200

    Signup

--- controller/signupController.php
+++ controller/signupController.php
@@ -22,3 +22,32 @@ function signupPage() {
 /***************************
 * ----- SIGNUP FUNCTION -----
 ***************************/
+
+function signup($post)
+{
+  if (isset($_POST['submit'])) :
+    $data           = new stdClass();
+    $data->email    = $post['email'];
+    $password = $post['password'];
+    $password_confirm = $post['password_confirm'];
+    $user           = new User($data);
+    $userMail       = $user->getUserByEmail($data->email);
+
+    echo $data->password . $data->password_confirm;
+    if ($password == $password_confirm) :
+      if ($user->getEmail() != $userMail['email']) :
+        $user->createUser();
+        $_SESSION['user_id'] = $userMail['id'];
+        header('location: index.php ');
+      else :
+        echo '<script type="text/javascript">alert("Cette adresse mail existe déjà");</script>';
+      endif;
+    else :
+      echo '<script type="text/javascript">alert("Votre mot de passe et la confirmation ne sont pas identique");</script>';
+    endif;
+  else :
+    echo '<script type="text/javascript">alert("Echec de la création de votre compte");</script>';
+  endif;
+
+  require('view/auth/signupView.php');
+}
